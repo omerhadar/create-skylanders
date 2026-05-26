5ecd2e1d295066526ca2bd6718c6ba672bf1123c
adari <omerhadar3@gmail.com>
2026-05-27 00:22:15 +0300

    oil

--- make_datapack.py
+++ make_datapack.py
@@ -7,33 +7,33 @@ import shutil
 
 TIERS = [
     # Tier 1 (Global - 100% chance everywhere)
-    ("zinc", [], "create:zinc_ore", "create:deepslate_zinc_ore", 0, 1500, 1.0, 12, 10),
-    ("coal", ["minecraft:ore_coal_upper", "minecraft:ore_coal_lower"], "minecraft:coal_ore", "minecraft:deepslate_coal_ore", 0, 1500, 1.0, 17, 10),
+    ("zinc", [], "create:zinc_ore", "create:deepslate_zinc_ore", 0, 1000, 1.0, 12, 10),
+    ("coal", ["minecraft:ore_coal_upper", "minecraft:ore_coal_lower"], "minecraft:coal_ore", "minecraft:deepslate_coal_ore", 0, 1000, 1.0, 17, 10),
     
     # Tier 2
-    ("copper", ["minecraft:ore_copper", "minecraft:ore_copper_large"], "minecraft:copper_ore", "minecraft:deepslate_copper_ore", 1000, 2000, 0.0, 12, 10),
-    ("quartz", [], "minecraft:nether_quartz_ore", "minecraft:nether_quartz_ore", 1000, 2000, 0.0, 14, 10),
-    ("gold", ["minecraft:ore_gold", "minecraft:ore_gold_lower"], "minecraft:gold_ore", "minecraft:deepslate_gold_ore", 1000, 2000, 0.0, 9, 10),
+    ("copper", ["minecraft:ore_copper", "minecraft:ore_copper_large"], "minecraft:copper_ore", "minecraft:deepslate_copper_ore", 2000, 3000, 0.0, 12, 10),
+    ("quartz", [], "minecraft:nether_quartz_ore", "minecraft:nether_quartz_ore", 2000, 3000, 0.0, 14, 10),
+    ("gold", ["minecraft:ore_gold", "minecraft:ore_gold_lower"], "minecraft:gold_ore", "minecraft:deepslate_gold_ore", 2000, 3000, 0.0, 9, 10),
     
     # Tier 3
-    ("iron", ["minecraft:ore_iron_upper", "minecraft:ore_iron_middle", "minecraft:ore_iron_small"], "minecraft:iron_ore", "minecraft:deepslate_iron_ore", 2500, 3500, 0.0, 12, 10),
-    ("lapis", ["minecraft:ore_lapis", "minecraft:ore_lapis_buried"], "minecraft:lapis_ore", "minecraft:deepslate_lapis_ore", 2500, 3500, 0.0, 7, 10),
+    ("iron", ["minecraft:ore_iron_upper", "minecraft:ore_iron_middle", "minecraft:ore_iron_small"], "minecraft:iron_ore", "minecraft:deepslate_iron_ore", 3500, 4500, 0.0, 12, 10),
+    ("lapis", ["minecraft:ore_lapis", "minecraft:ore_lapis_buried"], "minecraft:lapis_ore", "minecraft:deepslate_lapis_ore", 3500, 4500, 0.0, 7, 10),
     
     # Tier 4
-    ("lead", ["tfmg:lead_ore"], "tfmg:lead_ore", "tfmg:deepslate_lead_ore", 4000, 5000, 0.0, 10, 10),
-    ("bauxite", [], "tfmg:bauxite", None, 4000, 5000, 0.0, 24, 10),
+    ("lead", ["tfmg:lead_ore"], "tfmg:lead_ore", "tfmg:deepslate_lead_ore", 5000, 6000, 0.0, 10, 10),
+    ("bauxite", [], "tfmg:bauxite", None, 5000, 6000, 0.0, 24, 10),
     ("redstone", ["minecraft:ore_redstone", "minecraft:ore_redstone_lower"], "minecraft:redstone_ore", "minecraft:deepslate_redstone_ore", 0, 5000, 0.0, 8, 10),
-    ("nickel", ["tfmg:nickel_ore"], "tfmg:nickel_ore", "tfmg:deepslate_nickel_ore", 4000, 5000, 0.0, 10, 10), 
+    ("nickel", ["tfmg:nickel_ore"], "tfmg:nickel_ore", "tfmg:deepslate_nickel_ore", 5000, 6000, 0.0, 10, 10), 
     
     # Tier 5
-    ("diamond", ["minecraft:ore_diamond", "minecraft:ore_diamond_large", "minecraft:ore_diamond_buried"], "minecraft:diamond_ore", "minecraft:deepslate_diamond_ore", 5500, 6500, 0.0, 8, 10),
+    ("diamond", ["minecraft:ore_diamond", "minecraft:ore_diamond_large", "minecraft:ore_diamond_buried"], "minecraft:diamond_ore", "minecraft:deepslate_diamond_ore", 6500, 7500, 0.0, 8, 10),
     ("emerald", ["minecraft:ore_emerald"], "minecraft:emerald_ore", "minecraft:deepslate_emerald_ore", 5500, 6500, 0.0, 6, 10),
 
     # Create Custom "Strikes" (Massive dense resource blobs)
     ("asurine", [], "create:asurine", "create:asurine", 0, 2000, 0.0, 64, 2),
-    ("crimsite", [], "create:crimsite", "create:crimsite", 2500, 3500, 0.0, 64, 2),
-    ("ochrum", [], "create:ochrum", "create:ochrum", 1000, 5000, 0.0, 64, 2),
-    ("veridium", [], "create:veridium", "create:veridium", 1000, 2000, 0.0, 64, 2)
+    ("crimsite", [], "create:crimsite", "create:crimsite", 3500, 4500, 0.0, 64, 2),
+    ("ochrum", [], "create:ochrum", "create:ochrum", 2000, 3000, 0.0, 64, 2),
+    ("veridium", [], "create:veridium", "create:veridium", 2000, 3000, 0.0, 64, 2)
 ]
 
 NAMESPACE = "skylanders_ores"
@@ -71,7 +71,7 @@ for ore_name, feats_to_remove, stone_block, deepslate_block, min_dist, max_dist,
             "type": "neoforge:remove_features",
             "biomes": "#minecraft:is_overworld",
             "features": feats_to_remove,
-            "steps": "underground_ores"
+            "step": "underground_ores" # FIXED: Removed the 's' so it correctly parses in 1.21.1
         })
 
     write_json(os.path.join(neo_dir, f"add_{ore_name}.json"), {
@@ -135,99 +135,52 @@ write_json(os.path.join(neo_dir, "remove_tfmg_oil.json"), {
     "type": "neoforge:remove_features",
     "biomes": "#minecraft:is_overworld",
     "features": ["tfmg:oil_deposit"],
-    "steps": "underground_ores" 
+    "step": "underground_ores"  # FIXED: Removed the 's' here too!
 })
 
-# --- 5. GENERATE CUSTOM FLOATING OIL GEODES ---
+# --- 5. GENERATE THE NEW CUSTOM OIL METEORS ---
 write_json(os.path.join(neo_dir, "add_custom_oil.json"), {
     "type": "neoforge:add_features",
     "biomes": "#minecraft:is_overworld",
-    "features": [f"{NAMESPACE}:custom_oil_geode_sky"],
+    "features": [f"{NAMESPACE}:custom_oil_meteor_sky"],
     "step": "underground_ores"
 })
 
-write_json(os.path.join(conf_dir, "custom_oil_geode.json"), {
-  "type": "minecraft:geode",
+write_json(os.path.join(conf_dir, "custom_oil_meteor.json"), {
+  "type": "skylandersmeteors:meteor",
   "config": {
-    "blocks": {
-      "filling_provider": {
-        "type": "minecraft:weighted_state_provider",
-        "entries": [
-          {
-            "weight": 90, 
-            "data": { "Name": "tfmg:crude_oil" } 
-          },
-          {
-            "weight": 10,
-            "data": { "Name": "tfmg:oil_deposit" } 
-          }
-        ]
-      },
-      "inner_layer_provider": {
-        "type": "minecraft:simple_state_provider",
-        "state": { "Name": "minecraft:blackstone" }
-      },
-      "alternate_inner_layer_provider": {
-        "type": "minecraft:simple_state_provider",
-        "state": { "Name": "minecraft:magma_block" }
-      },
-      "middle_layer_provider": {
-        "type": "minecraft:simple_state_provider",
-        "state": { "Name": "minecraft:smooth_basalt" }
-      },
-      "outer_layer_provider": {
-        "type": "minecraft:simple_state_provider",
-        "state": { "Name": "minecraft:tuff" }
-      },
-      
-      # FIXED: Uses exact JSON objects instead of strings
-      "inner_placements": [ { "Name": "tfmg:oil_deposit" }, { "Name": "tfmg:crude_oil" } ], 
-      
-      "cannot_replace": "#minecraft:features_cannot_replace",
-      "invalid_blocks": "#minecraft:geode_invalid_blocks"
+    "outer_shell": {
+      "type": "minecraft:simple_state_provider",
+      "state": { "Name": "minecraft:tuff" }
     },
-    
-    "layers": {
-      "filling": 6.0,
-      "inner_layer": 7.0,
-      "middle_layer": 8.0,
-      "outer_layer": 9.0
+    "middle_shell": {
+      "type": "minecraft:simple_state_provider",
+      "state": { "Name": "minecraft:smooth_basalt" }
     },
-    "crack": {
-      "generate_crack_chance": 0.0, 
-      "base_crack_size": 2.0,
-      "crack_point_offset": 2
+    "inner_shell": {
+      "type": "minecraft:simple_state_provider",
+      "state": { "Name": "minecraft:blackstone" }
     },
-    "noise_multiplier": 0.05,
-    "use_potential_placements_chance": 0.0,
-    "use_alternate_layer0_chance": 0.1,
-    "placements_require_layer0_alternate": False, 
-    
-    # FIXED: Re-included the "value" wrapper for the IntProviders
-    "outer_wall_distance": {
+    "filling": {
+      "type": "minecraft:simple_state_provider",
+      "state": { "Name": "tfmg:crude_oil" } 
+    },
+    "floor": {
+      "type": "minecraft:simple_state_provider",
+      "state": { "Name": "tfmg:oil_deposit" }
+    },
+    "radius": {
       "type": "minecraft:uniform",
-      "value": { "min_inclusive": 8, "max_inclusive": 16 }
-    },
-    "distribution_points": {
-      "type": "minecraft:uniform",
-      "value": { "min_inclusive": 3, "max_inclusive": 6 }
-    },
-    "point_offset": {
-      "type": "minecraft:uniform",
-      "value": { "min_inclusive": 1, "max_inclusive": 3 }
-    },
-    
-    "min_gen_offset": -16,
-    "max_gen_offset": 16,
-    
-    "invalid_blocks_threshold": 100000 
+      "min_inclusive": 10,
+      "max_inclusive": 16 
+    }
   }
 })
 
-write_json(os.path.join(place_dir, "custom_oil_geode_sky.json"), {
-  "feature": f"{NAMESPACE}:custom_oil_geode",
+write_json(os.path.join(place_dir, "custom_oil_meteor_sky.json"), {
+  "feature": f"{NAMESPACE}:custom_oil_meteor",
   "placement": [
-    { "type": "minecraft:rarity_filter", "chance": 250 }, 
+    { "type": "minecraft:rarity_filter", "chance": 1000 }, 
     { "type": "minecraft:in_square" },
     {
       "type": "minecraft:height_range",
@@ -263,4 +216,4 @@ with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
             zipf.write(file_path, os.path.relpath(file_path, PACK_DIR))
 
 shutil.rmtree(PACK_DIR)
-print(f"Successfully created {zip_filename} with correct Geode syntax!")
+print(f"Successfully created {zip_filename} with fixed removal syntax!")
